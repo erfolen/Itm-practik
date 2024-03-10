3f3688c18bc489c30112bd486bbc2035219d6ae2
videbul <videbul@gmail.com>
2024-03-10 11:36:28 +0300

    fixed project

--- 8_SQLAlchemy/main.py
+++ 8_SQLAlchemy/main.py
@@ -3,31 +3,31 @@ from data import employees_data, shippers_data, supplies_data, products_data, cu
 from models import Employees, Shippers, Supplies, Products, Customers, Orders
 
 if __name__ == '__main__':
-    # ActionsDB.drop_table()
-    # ActionsDB.create_table()
-    # # ActionsDB.insert_data_one(employees_data_1)
-    # # ActionsDB.insert_data_all([employees_data_2, employees_data_3,
+    ActionsDB.drop_table()
+    ActionsDB.create_table()
+    # ActionsDB.insert_data_one(employees_data_1)
+    # ActionsDB.insert_data_all([employees_data_2, employees_data_3,
     # employees_data_4, employees_data_5, employees_data_6])
-    ## Ввод данных
-    # ActionsDB.insert_data_all(employees_data)
-    # ActionsDB.insert_data_all(shippers_data)
-    # ActionsDB.insert_data_all(supplies_data)
-    # ActionsDB.insert_data_all(products_data)
-    # ActionsDB.insert_data_all(customers_data)
-    # ActionsDB.insert_data_all(orders_data)
-    ## Вывод данных
-    # employees = ActionsDB.select_all_table(Employees)
-    # ActionsDB.print_properties(employees)
-    # shippers = ActionsDB.select_all_table(Shippers)
-    # ActionsDB.print_properties(shippers)
-    # supplies = ActionsDB.select_all_table(Supplies)
-    # ActionsDB.print_properties(supplies)
-    # products = ActionsDB.select_all_table(Products)
-    # ActionsDB.print_properties(products)
-    # customers = ActionsDB.select_all_table(Customers)
-    # ActionsDB.print_properties(customers)
-    # orders = ActionsDB.select_all_table(Orders)
-    # ActionsDB.print_properties(orders)
+    # Ввод данных
+    ActionsDB.insert_data_all(employees_data)
+    ActionsDB.insert_data_all(shippers_data)
+    ActionsDB.insert_data_all(supplies_data)
+    ActionsDB.insert_data_all(products_data)
+    ActionsDB.insert_data_all(customers_data)
+    ActionsDB.insert_data_all(orders_data)
+    # Вывод данных
+    employees = ActionsDB.select_all_table(Employees)
+    ActionsDB.print_properties(employees)
+    shippers = ActionsDB.select_all_table(Shippers)
+    ActionsDB.print_properties(shippers)
+    supplies = ActionsDB.select_all_table(Supplies)
+    ActionsDB.print_properties(supplies)
+    products = ActionsDB.select_all_table(Products)
+    ActionsDB.print_properties(products)
+    customers = ActionsDB.select_all_table(Customers)
+    ActionsDB.print_properties(customers)
+    orders = ActionsDB.select_all_table(Orders)
+    ActionsDB.print_properties(orders)
     # вывод join
     join_supplies = ActionsDB.join_table(Shippers, Supplies)
     for row in join_supplies:
--- 9_FastAPI/Fastapi_project/config_my.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-login = os.getenv("user_database")
-password = os.getenv("PASSWORD")
-database = os.getenv("DATABASE")
-host = os.getenv("HOST")
