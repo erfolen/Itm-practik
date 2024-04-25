99bb33300fda97ec98804f6a81285cb7e6961a73
videbul <videbul@gmail.com>
2024-04-25 23:23:10 +0300

    create django project and create models

---
+++ shop_django/shop/main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from main.models import Categories, Manufactures, Products
+
+admin.site.register(Categories)
+admin.site.register(Manufactures)
+admin.site.register(Products)
