e9c072170be5694e017faba40f8df8395822a6c6
videbul <videbul@gmail.com>
2024-05-04 08:28:07 +0300

    create timermiddleware

---
+++ shop_django/shop/shop/middlewares.py
@@ -0,0 +1,15 @@
+import time
+
+
+class TimerMiddleware:
+    def __init__(self, get_response: callable):
+        self.get_response = get_response
+
+    def __call__(self, request, *args, **kwargs):
+        #Before response
+        start_time = time.monotonic()
+        response = self.get_response(request, *args, **kwargs)  #callae view
+        #After response
+        end_time = time.monotonic()
+        print('time: ', end_time - start_time)
+        return response
--- shop_django/shop/shop/settings.py
+++ shop_django/shop/shop/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'shop.middlewares.TimerMiddleware',
 ]
 
 ROOT_URLCONF = 'shop.urls'
