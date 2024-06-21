e3dcd6ce205fbd7caeb69559c5a59d25fbd7eb13
videbul <videbul@gmail.com>
2024-06-21 20:57:02 +0300

    settings chat

--- shop_django/shop/shop/urls.py
+++ shop_django/shop/shop/urls.py
@@ -21,11 +21,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls', namespace='user')),
+    path('chat/', include('chat.urls', namespace='chat')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/products/', include('main.urls'), name='products'),
     path('api/v1/cart/', include('cart.urls'), name='cart'),
-    path("__debug__/", include("debug_toolbar.urls"))
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
