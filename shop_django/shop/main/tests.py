1075195b65227b092e8973d1d642e8ddd0f937bb
videbul <videbul@gmail.com>
2024-06-27 23:12:30 +0300

    creanit init test

--- shop_django/shop/main/tests.py
+++ shop_django/shop/main/tests.py
@@ -1,5 +1,11 @@
+from rest_framework.test import APITestCase
 from django.test import TestCase
+from .models import Categories, Products, Manufactures
+
+class CategoriesTest(TestCase):
+    pass
 
 
-
-# Create your tests here.
+class ProductTest(APITestCase):
+    def setUp(self):
+        Products.objects.create()
