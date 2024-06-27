771e240675153fadaf3fe77b365126c3ed024e6c
videbul <videbul@gmail.com>
2024-06-27 21:56:50 +0300

    create async chaat

--- shop_django/shop/shop/settings.py
+++ shop_django/shop/shop/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'user',
     'cart',
     'debug_toolbar',
-
 ]
 
 MIDDLEWARE = [
@@ -52,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- shop_django/shop/shop/urls.py
+++ shop_django/shop/shop/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
